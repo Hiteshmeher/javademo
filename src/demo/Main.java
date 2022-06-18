63481cb13739dce2539a35e5e4b1fe523c592459
Hitesh Kumar Meher <107098715+Hiteshmeher@users.noreply.github.com>
2022-06-18 21:13:21 +0530

    Update Main.java

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hi i am Java2 Branch");
+		System.out.println("Hi i am Java3 Branch");
 		// TODO Auto-generated method stub
 
 	}
