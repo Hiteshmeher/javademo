c77564172b05025891c896a0c08eb3b6357ad81d
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-18 21:15:10 +0530

    name added

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hi i am Java2 Branch");
+		System.out.println("Hi i am Hitesh");
 		// TODO Auto-generated method stub
 
 	}
